3df2234103da3c7a62221aaf8ad1dd3db4059cc1
MichelleFdo <mufernando02@gmail.com>
2020-05-23 03:32:44 +0530

    Q@

--- Assignment 01/src/Q01.scala
+++ Assignment 01/src/Q01.scala
@@ -1,5 +1,5 @@
 object Q01 {
-  def faranhiteConvertor(c: Int) = {
+  def faranhiteConvertor(c: Int): Unit = {
     print(((c*1.8000)+32.00)+" F")
   }
 
---
+++ Assignment 01/src/Q02.scala
@@ -0,0 +1,8 @@
+object Q02 {
+  def findVolume(r: Float) = {
+    print("volume is : "+((4*22*r*r*r)/(3*7)))
+  }
+  def main()=
+    {}
+  findVolume(5.0)
+}
