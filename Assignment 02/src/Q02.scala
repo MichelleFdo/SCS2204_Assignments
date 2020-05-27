7ab7cf6dae7b010c617126fe6afc6b04455dc0b4
MichelleFdo <mufernando02@gmail.com>
2020-05-28 00:25:41 +0530

    Q02 final

--- Assignment 02/src/Q02.scala
+++ Assignment 02/src/Q02.scala
@@ -1,11 +1,18 @@
 object Q02 extends App{
   val variationForOneRupee =20/5
+  def count(price:Double)                  : Double = 120-(price-15)*variationForOneRupee
+  def income(price:Double)                 : Double = count(price)*price
+  def cost(price:Double)                   : Double = 500+count(price)*3
+  def profit(price:Double)                 : Double = income(price) - cost(price)
+  def betterPrice(): Unit = {
+    var i=5;
+    while (i<50)
+    {
+      println("Count  Price   income    cost    profit")
+      println(count(i), "   ", i, "   ", income(i), "  ", cost(i), "   ", profit(i))
+      i+=5
+    }
 
-  def income(n:Int, price:Double)          : Double = n*price
-  def cost(n:Int)                          : Double = 500+n*3
-  def profit(n: Int,price:Double)          : Double = income(n,price) - cost(n)
-  println("income is",income(120,15))
-  println("cost is",cost(120))
-  println("profit is",profit(120,15))
-  def findBest(profit:Double,cost:Double)  : Double =
+  }
+  betterPrice()
 }
