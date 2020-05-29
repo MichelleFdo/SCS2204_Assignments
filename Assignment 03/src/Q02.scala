6fe1b79dacbf78c75e2b769a02c349625e16f39a
MichelleFdo <mufernando02@gmail.com>
2020-05-29 21:29:04 +0530

    n=10

--- Assignment 03/src/Q02.scala
+++ Assignment 03/src/Q02.scala
@@ -1,10 +1,5 @@
 object Q02 extends App {
 
-    def primeSeq(n:Int,x:Int=2):Unit = {
-      if (prime(x)) print(x+",")
-      if (x==n) return
-      primeSeq(n,x+1)
-    }
     def gcd(a:Int,b:Int):Int=b match{
       case 0 => a
       case b if (b>a) => gcd(b,a)
@@ -15,6 +10,12 @@ object Q02 extends App {
       case x if gcd(n,x)>1 => false
       case x => prime(n,x+1)
     }
-    primeSeq(5)
+    def primeSeq(n:Int,x:Int=2):Unit = {
+      if (prime(x)) print(x+",")
+      if (x==n) return
+      primeSeq(n,x+1)
+    }
+
+  primeSeq(10)
 
 }
