9d1a68f207eae9f4b33fa826f2c2073fc4938fcf
MichelleFdo <mufernando02@gmail.com>
2020-05-29 19:04:13 +0530

    find prime

--- Assignment 03/src/Q01.scala
+++ Assignment 03/src/Q01.scala
@@ -1,14 +1,16 @@
 object Q01 extends App{
-  def  prime(p:Int,n:Int=2):Boolean= n match {
+  def  prime(p:Int,x:Int=2):Boolean= x match {
     case x if(x==p) => true
     case x if GCD(p,x)>1 => false
     case x => prime(p,x+1)
   }
   def GCD(a:Int,b:Int):Int=b match{
     case 0 => a
-    case x if x>a => GCD(x,a)
-    case _ => GCD(x,a%x)
+    case b if b>a => GCD(b,a)
+    case _ => GCD(b,a%b)
   }
 
-  def prime(8)
+  println("Is 8 a prime: ",prime(8))
+  println("Is 2 a prime: ",prime(2))
+  println("Is 7 a prime: ",prime(7))
 }
